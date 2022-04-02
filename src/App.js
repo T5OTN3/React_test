02e30438a051b58e0cede7417e524ec180c76a74
t5otn3 <cotne1989@gmail.com>
2022-04-03 00:55:21 +0400

    Add popup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, useMemo } from 'react';
 import mapboxgl from 'mapbox-gl';
-import Map, {Popup, ScaleControl, Source, Layer, GeolocateControl, NavigationControl, FullscreenControl} from 'react-map-gl';
+import Map, {Popup, ScaleControl, Marker, Source, Layer, GeolocateControl, NavigationControl, FullscreenControl} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import Pin from './components/map/pin';
+import CITIES from './components/map/data.json'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY290bmUxOTg5IiwiYSI6ImNqcmcxZWxnNDFqdzU0YW1seW43ZDZ1NjgifQ.zX_zd9Fx1zxv0Yo4gBCu_Q';
 
@@ -23,7 +25,22 @@ const layerStyle = {
 
 function App() {
   const [showPopup, setShowPopup] = useState(true);
+  const [popupInfo, setPopupInfo] = useState(null);
 
+  const pins = useMemo(
+    () =>
+      CITIES.map((city, index) => (
+        <Marker
+          key={`marker-${index}`}
+          longitude={Number(city.lng)}
+          latitude={Number(city.lat)}
+          anchor="bottom"
+        >
+          <Pin onClick={() => setPopupInfo(city)} />
+        </Marker>
+      )),
+    []
+  );
 
   return (
     <Map
@@ -35,11 +52,15 @@ function App() {
         style={{width: 600, height: 400}}
         mapStyle="mapbox://styles/mapbox/streets-v9"
       >
-        {showPopup && (
-          <Popup longitude={42.7} latitude={42.30}
+        {pins}
+        {popupInfo && (
+          <Popup 
+            longitude={Number(popupInfo.lng)} 
+            latitude={Number(popupInfo.lat)}
             anchor="bottom"
-            onClose={() => setShowPopup(false)}>
-            <p className='text-lg text-orange-500'>You are here</p> 
+            closeOnClick={false}
+            onClose={() => setPopupInfo(null)}>
+            <p className='text-lg text-orange-500'>{popupInfo.name}</p> 
           </Popup>)}
           <ScaleControl />
           <Source id="my-data" type="geojson" data={geojson}>
